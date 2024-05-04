ff29d5e2420e43d6d2912b3c337e444deae4b4ea
PitSheep <pitsheep@gmail.com>
2024-05-04 10:52:09 +0200

    HUNTER HOUSE draft
    Hunter House Draft

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1058,3 +1058,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/birch_speech.inc"
 
 	.include "data/maps/OldaleTown_Gym/scripts.inc"
+
+	.include "data/maps/OldaleTown_Hunter_House/scripts.inc"
